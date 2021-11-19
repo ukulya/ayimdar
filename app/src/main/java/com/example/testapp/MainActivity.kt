ffc5925b013ee2f927c078578c702396bcc16a6f
ukulya <ukulya150992@gmail.com>
2021-11-19 17:31:57 +0600

    first commit

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -4,30 +4,15 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.testapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
 
-    private val btn3 = binding.btn3
-
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.btn1.text = "new btn bind"
-
-        // мы внутри этого объекта
-        binding.apply {
-            btn2.text = "new btn bind 2"
-            btn4.setOnClickListener{
-            startActivity(
-                    Intent(this@MainActivity,MainActivity2::class.java)
-                )
-            }
-        }
+        val recycler = findViewById<RecyclerView>(R.id.recycler)
     }
 }
---
+++ app/src/main/java/com/example/testapp/SimpleAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.testapp
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import android.view.ViewGroup
+
+class SimpleAdapter: RecyclerView.Adapter<SimpleAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SimpleAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+    }
+}

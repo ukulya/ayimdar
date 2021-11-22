db23228b625863b176cb67cb7f0a04c88d5e6a86
ukulya <ukulya150992@gmail.com>
2021-11-22 17:22:08 +0600

    first commit

--- app/src/main/java/com/example/testapp/SimpleAdapter.kt
+++ app/src/main/java/com/example/testapp/SimpleAdapter.kt
@@ -1,24 +1,10 @@
 package com.example.testapp
 
-import android.view.View
+import android.widget.Adapter
 import androidx.recyclerview.widget.RecyclerView
-import android.view.ViewGroup
 
-class SimpleAdapter: RecyclerView.Adapter<SimpleAdapter.ViewHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SimpleAdapter.ViewHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-
-    }
+class SimplerAdapter : RecyclerView.Adapter<Adapter.ViewHolder>() {
+//    private val ViewHolder =
+//    override fun onCreatedV
+        //class ViewHolder =
 }

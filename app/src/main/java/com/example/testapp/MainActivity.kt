b8919cbd0c01124440d1e51be1dc6ba893b3c32a
ukulya <ukulya150992@gmail.com>
2021-11-28 22:12:24 +0600

    first commit

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -1,111 +1,55 @@
 package com.example.testapp
 
+
+import android.annotation.SuppressLint
+import android.content.ClipDescription
 import android.content.Intent
-import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import android.os.Message
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
-import androidx.core.view.ViewCompat.setBackground
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import net.objecthunter.exp4j.ExpressionBuilder
+import androidx.appcompat.widget.AppCompatEditText
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    // TextView used to display the input and output
-    lateinit var txtInput: TextView
 
-    // Represent whether the lastly pressed key is numeric or not
-    var lastNumeric: Boolean = false
-
-    // Represent that current state is in error or not
-    var stateError: Boolean = false
-
-    // If true, do not allow to add another DOT
-    var lastDot: Boolean = false
+    val email = findViewById<AppCompatEditText>(R.id.email)
+    val subject = findViewById<AppCompatEditText>(R.id.subject)
+    val text = findViewById<AppCompatEditText>(R.id.text)
+    val btn = findViewById<AppCompatButton>(R.id.btn)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        txtInput = findViewById(R.id.txtInput)
-    }
-    /**
-    * Append the Button.text to the TextView
-    */
-    fun onDigit(view: View) {
-        if (stateError) {
-            // If current state is Error, replace the error message
-            txtInput.text = (view as Button).text
-            stateError = false
-        } else {
-            // If not, already there is a valid expression so append to it
-            txtInput.append((view as Button).text)
+
+        btn.setOnClickListener {
+            val recipient = email.text.toString().trim()
+            val subj = subject.text.toString().trim()
+            val message = text.text.toString().trim()
+            sendEmail(recipient,subj,message)
         }
-        // Set the flag
-        lastNumeric = true
+
     }
 
-    /**
-     * Append . to the TextView
-     */
-    fun onDecimalPoint(view: View) {
-        if (lastNumeric && !stateError && !lastDot) {
-            txtInput.append(".")
-            lastNumeric = false
-            lastDot = true
+    private fun sendEmail(recipient: String,subj: String,message: String) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.data = Uri.parse("mailto:")
+        intent.type = "plain/text"
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipient))
+        intent.putExtra(Intent.EXTRA_SUBJECT, subj)
+        intent.putExtra(Intent.EXTRA_TEXT, message)
+
+        try {
+            startActivity(Intent.createChooser(intent,"Choose email client"))
+        } catch (e: Exception){
+            Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
         }
-    }
 
-    /**
-     * Append +,-,*,/ operators to the TextView
-     */
-    fun onOperator(view: View) {
-        if (lastNumeric && !stateError) {
-            txtInput.append((view as Button).text)
-            lastNumeric = false
-            lastDot = false    // Reset the DOT flag
-        }
+
     }
 
 
-    /**
-     * Clear the TextView
-     */
-    fun onClear(view: View) {
-        this.txtInput.text = ""
-        lastNumeric = false
-        stateError = false
-        lastDot = false
-    }
 
-    /**
-     * Calculate the output using Exp4j
-     */
-    fun onEqual(view: View) {
-        // If the current state is error, nothing to do.
-        // If the last input is a number only, solution can be found.
-        if (lastNumeric && !stateError) {
-            // Read the expression
-            val txt = txtInput.text.toString()
-            // Create an Expression (A class from exp4j library)
-            val expression = ExpressionBuilder(txt).build()
-            try {
-                // Calculate the result and display
-                val result = expression.evaluate()
-                txtInput.text = result.toString()
-                lastDot = true // Result contains a dot
-            } catch (ex: ArithmeticException) {
-                // Display an error message
-                txtInput.text = "Error"
-                stateError = true
-                lastNumeric = false
-            }
-        }
-    }
 }

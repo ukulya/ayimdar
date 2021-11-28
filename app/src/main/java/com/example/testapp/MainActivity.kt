9d625099156868a21137a5fab40163d9f95d9ec2
ukulya <ukulya150992@gmail.com>
2021-11-28 18:34:10 +0600

    first commit

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -13,30 +13,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //val item = findViewById<AppCompatButton>(R.id.btn_1)
-
-        val recycler = findViewById<RecyclerView>(R.id.recycler)
-        val layoutManager = LinearLayoutManager(this)
-        val adapter = SimpleAdapter {
-            Toast.makeText(this, "ITEM -$it", Toast.LENGTH_SHORT).show()
-            goTo(it)
-        }
-
-        recycler.layoutManager = layoutManager
-        recycler.adapter = adapter
-        recycler.addItemDecoration(DividerItemDecoration(this, RecyclerView.VERTICAL))
-
-        val list = mutableListOf<String>()
-        for (i in 0..20) {
-            list.add("ITEM -$i")
-        }
-        adapter.setData(list)
-    }
-
-    private fun goTo(position: Int) {
-        val intent = Intent(this,MainActivity2::class.java) // явный - явно указываем; неявный - через manifest
-        intent.putExtra("item", position) // ключ - значение
-        startActivity(intent)
     }
 }

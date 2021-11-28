bd0a68a2bed41168258d67e1d81b4dc14184f711
ukulya <ukulya150992@gmail.com>
2021-11-28 20:31:12 +0600

    first commit

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
@@ -13,33 +15,97 @@ import androidx.core.view.ViewCompat.setBackground
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var txtView: TextView
-    var counter = 1
+    // TextView used to display the input and output
+    lateinit var txtInput: TextView
+
+    // Represent whether the lastly pressed key is numeric or not
+    var lastNumeric: Boolean = false
+
+    // Represent that current state is in error or not
+    var stateError: Boolean = false
+
+    // If true, do not allow to add another DOT
+    var lastDot: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        txtView = findViewById(R.id.text)
-
-
-        val btn1 = findViewById<AppCompatButton>(R.id.btn1)
-        val btn2 = findViewById<AppCompatButton>(R.id.btn2)
-
-
-        btn1.setOnClickListener {
-            txtView.text = counter++.toString()
-
+        txtInput = findViewById(R.id.txtInput)
+    }
+    /**
+    * Append the Button.text to the TextView
+    */
+    fun onDigit(view: View) {
+        if (stateError) {
+            // If current state is Error, replace the error message
+            txtInput.text = (view as Button).text
+            stateError = false
+        } else {
+            // If not, already there is a valid expression so append to it
+            txtInput.append((view as Button).text)
         }
+        // Set the flag
+        lastNumeric = true
+    }
 
-        btn2.setOnClickListener {
-            txtView.text = "0"
-            counter = 1
+    /**
+     * Append . to the TextView
+     */
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !stateError && !lastDot) {
+            txtInput.append(".")
+            lastNumeric = false
+            lastDot = true
         }
+    }
+
+    /**
+     * Append +,-,*,/ operators to the TextView
+     */
+    fun onOperator(view: View) {
+        if (lastNumeric && !stateError) {
+            txtInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false    // Reset the DOT flag
+        }
+    }
 
 
+    /**
+     * Clear the TextView
+     */
+    fun onClear(view: View) {
+        this.txtInput.text = ""
+        lastNumeric = false
+        stateError = false
+        lastDot = false
+    }
 
+    /**
+     * Calculate the output using Exp4j
+     */
+    fun onEqual(view: View) {
+        // If the current state is error, nothing to do.
+        // If the last input is a number only, solution can be found.
+        if (lastNumeric && !stateError) {
+            // Read the expression
+            val txt = txtInput.text.toString()
+            // Create an Expression (A class from exp4j library)
+            val expression = ExpressionBuilder(txt).build()
+            try {
+                // Calculate the result and display
+                val result = expression.evaluate()
+                txtInput.text = result.toString()
+                lastDot = true // Result contains a dot
+            } catch (ex: ArithmeticException) {
+                // Display an error message
+                txtInput.text = "Error"
+                stateError = true
+                lastNumeric = false
+            }
+        }
     }
 }

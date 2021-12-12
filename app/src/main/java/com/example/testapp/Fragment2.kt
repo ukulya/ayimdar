5f28a58d9a8b8f9f03ea3792ad9d57e460f45376
ukulya <ukulya150992@gmail.com>
2021-12-13 00:04:36 +0600

    fisrst commit

---
+++ app/src/main/java/com/example/testapp/Fragment2.kt
@@ -0,0 +1,11 @@
+package com.example.testapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class Fragment2: Fragment(R.layout.fragment2) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.testapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var toolbar: Toolbar
     private lateinit var emailInput: EditText
     private lateinit var passInput: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +41,9 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
     }
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
@@ -48,5 +55,22 @@ class MainActivity : AppCompatActivity() {
         super.onRestoreInstanceState(savedInstanceState)
         //emailInput.text = savedInstanceState.getString("email")
         //passInput.text = savedInstanceState.getString("key")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        //menu?.add("menu 1")
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //Toast.makeText(this,item.title,Toast.LENGTH_SHORT).show()
+        toolbar.title = item.title
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, Fragment2())
+            .addToBackStack(null)
+            .commit()
+        findViewById<AppCompatButton>(R.id.btn).visibility = View.INVISIBLE
+        return super.onOptionsItemSelected(item)
     }
 }

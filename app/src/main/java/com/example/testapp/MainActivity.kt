bb690b04e6264e3fa6ba849a353dc2286e7377e0
ukulya <48289352+ukulya@users.noreply.github.com>
2021-12-14 20:58:53 +0600

    Update MainActivity.kt

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -5,14 +5,10 @@ import android.os.Bundle
 import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity(),OnButtonClicked {
-    private lateinit var txt1: AppCompatTextView
-    private lateinit var txt2: AppCompatTextView
+   
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //txt1 = findViewById(R.id.frg1_txt)
-        //txt2 = findViewById(R.id.frg2_txt)
 
         supportFragmentManager.beginTransaction()
             .add(R.id.fragment_container_1, Fragment1())

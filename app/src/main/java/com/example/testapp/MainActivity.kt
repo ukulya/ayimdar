d8fe847fc899e10c9bc52ee286f434a05c5bc4cc
ukulya <ukulya150992@gmail.com>
2021-12-22 18:50:09 +0600

    first commit

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.testapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            getPreferences(MODE_PRIVATE)
+            val prefs = getSharedPreferences("MyAppPrefs", MODE_PRIVATE)
+
+            txt.text = prefs.getString(KEY_TEXT, "defValue")
+            txt.text = prefs.getString(KEY_TEXT_2, "defValue")
+
+            btn.setOnClickListener {
+                val editor = prefs.edit()
+                editor.putString(KEY_TEXT, edit.text.toString())
+                editor.apply()
+            }
+
+            btn.setOnClickListener {
+                val editor = prefs.edit()
+                editor.putString(KEY_TEXT_2, edit.text.toString())
+                editor.apply()
+            }
+        }
+    }
+
+    companion object {
+        private const val KEY_TEXT = "keyText"
+        private const val KEY_TEXT_2 = "keyText2"
     }
 }

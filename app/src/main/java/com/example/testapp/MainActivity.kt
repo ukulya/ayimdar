b108d9c00987fad4066646893329883975535d1b
ukulya <ukulya150992@gmail.com>
2021-12-27 18:14:50 +0600

    first commit

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.example.testapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var input: AppCompatEditText
+    private lateinit var text: AppCompatTextView
+    private lateinit var btn: AppCompatButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        input = findViewById(R.id.edit)
+        text = findViewById(R.id.text)
+        btn = findViewById(R.id.btn)
+        btn.setOnClickListener {
+
+            val count = Regex("""(\s+|(\r\n|\r|\n))""").findAll(input.text.toString().trim()).count() + 1
+            text.text = count.toString()
+        }
     }
 }

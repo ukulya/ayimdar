6a84c670867b0009c46a283c233071c9af4d9f0b
ukulya <ukulya150992@gmail.com>
2021-12-29 18:41:51 +0600

    init less 36

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -1,39 +1,31 @@
 package com.example.testapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var launcher: ActivityResultLauncher<Intent>
     private lateinit var text: AppCompatTextView
+    private lateinit var input: AppCompatEditText
     private lateinit var btn: AppCompatButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        registerLauncher()
 
         text = findViewById(R.id.text)
-
+        input = findViewById(R.id.edit)
         btn = findViewById(R.id.btn)
 
         btn.setOnClickListener {
-            launcher.launch(Intent(this, MainActivity2::class.java))
+            text.text = input.text.toString().split("\\P{L}+".toRegex())
+                .filter { it.endsWith("ая") }
+                .size
+                .toString()
         }
     }
-    private fun registerLauncher() {
-        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            if (it.resultCode == RESULT_OK) {
-                text.text = it.data?.getStringExtra("editText") ?: "default"
-            }
-        }
-    }
+
 
 }
